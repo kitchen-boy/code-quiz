852c8fe2df1a60a8f80b2e002e5aa46650dfb994
kitchen-boy <kaye.chen.boyce@gmail>
2021-11-15 18:27:44 -0500

    JS Start over

--- assets/css/js/game.js
+++ assets/css/js/game.js
@@ -1,13 +1,3 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionsCounter = 0
-let availableQuestions = []
 
 let questions = [
     {
@@ -82,66 +72,6 @@ let questions = [
     }
 ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
 
-startGame = () => {
-    questionsCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
-
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore',score)
-
-        return window.location.assign('/end.html')
     }
     questionsCounter++
-    progressText.innerText= `Question ${questionsCounter} of ${MAX_QUESTIONS}`
-
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-}  
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset ['number']
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
-        'incorrect'
-
-        if(classToApply === 'correct') {
-            incrementScore(SCORE_POINTS)
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply)
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-
-    })
-})
-
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
-}
-
-startGame()
